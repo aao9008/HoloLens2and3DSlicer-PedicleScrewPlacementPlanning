2842ac52ab4b1785ca4833a9e7c7e34a24b1fc85
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-12-06 19:30:27 -0500

    Enhancement: Extract and log names of models from glTF file
    Added functionality to the PatientModelsFetcher script to extract model names from a glTF file. Implemented functions for parsing glTF data, filtering nodes with mesh, and logging their names.

--- AR_Planner-Unity/Assets/Scripts/PatientInfoFetcher/PatientModelsFetcher.cs
+++ AR_Planner-Unity/Assets/Scripts/PatientInfoFetcher/PatientModelsFetcher.cs
@@ -15,8 +15,24 @@ public class PatientModelsFetcher : MonoBehaviour
         modelsFolderPath = GetModelsPath(patientID); // Returns full path to patients folder (EX: Assets/Models/Patient 01)
 
         patientModelsArray = Directory.GetFiles(modelsFolderPath, "*.obj");
+
+        if (patientModelsArray.Length < 1)
+        {
+           patientModelsArray = Directory.GetFiles(modelsFolderPath, "*.gltf");
+
+            // Check if a GLTF file exists
+            if (patientModelsArray.Length < 1)
+            {
+                return patientModelsArray;
+            }
+
+            string gltfPath = patientModelsArray[0];
+
+            patientModelsArray = GetChildNamesFromGLTF(gltfPath);
+        }
+
         patientModelsArray = patientModelsArray.Select(model => Path.GetFileNameWithoutExtension(model)).Distinct().ToArray();
- 
+
         return patientModelsArray;
     }
 
@@ -39,4 +55,50 @@ public class PatientModelsFetcher : MonoBehaviour
 
         return modelMTLPath;
     }
+
+    private static string[] GetChildNamesFromGLTF(string gltfPath)
+    {
+        string jsonText = File.ReadAllText(gltfPath);
+
+        // Parse the JSON text into an object
+        GLTFData gltfData = JsonUtility.FromJson<GLTFData>(jsonText);
+
+        // Get node names
+        List<string> modelNames = GetModelNames(gltfData.nodes);
+
+        // Convert list to array and return model names
+        return modelNames.ToArray();
+    }
+
+    private static List<string> GetModelNames(List<GLTFNode> nodes)
+    {
+        List<string> modelNames = new List<string>();
+
+        foreach (var node in nodes)
+        {
+            
+             if (node.mesh != null)
+            {
+                modelNames.Add(node.name);
+            }
+ 
+        }
+
+        return modelNames;
+    }
 }
+
+[System.Serializable]
+public class GLTFData
+{
+    public List<GLTFNode> nodes;
+}
+
+[System.Serializable]
+public class GLTFNode
+{
+    public string mesh;
+    public string name;
+}
+
+    
