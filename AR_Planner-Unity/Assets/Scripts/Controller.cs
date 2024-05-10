14b01d58182b68c7569753c1c271f04e34acba5d
Alfredo Ormeno <ormenoandres13@gmail.com>
2024-05-10 10:40:00 -0400

    Implement locking and unlocking functionality on game-pad controller
    This requires holding the B button on the Xbox controller
    for 2 seconds to toggle lock/unlock model status.

--- AR_Planner-Unity/Assets/Scripts/Controller.cs
+++ AR_Planner-Unity/Assets/Scripts/Controller.cs
@@ -8,17 +8,23 @@ public class Controller : MonoBehaviour
     // Boolean variable to keep track of model visibility state
     private bool modelOn = true;
     public SwitchButtons switchButtons;
-    private bool buttonHeldDown = false;
-    private float holdStartTime = 0f;
-    private float holdDurationThreshold = 2f; // Adjust this threshold as needed. 
-    private bool actionExecuted = false;
+    public PressableButtons pressableButtons;
+    //GameObject model;
+
 
     public AudioSource buttonDown;
     public AudioSource buttonUp;
+
+    bool buttonHeldDown = false;
+    float holdStartTime = 0f;
+    float holdDurationThreshold = 2f; // Adjust this threshold as needed. 
+    bool actionExecuted = false;
+    bool modelLocked = false;
     
     void Update()
     {
         ToggleParentModel();
+        LockBody();
     }
 
     // This function handels logic for parent model visiblity controller button
@@ -47,6 +53,61 @@ public class Controller : MonoBehaviour
         }
 
         modelOn= modelOn ? false : true;
+    }
+
+    void LockBody()
+    {
+       if (Input.GetButton("LockBody"))
+       {
+           // Button is being held down
+           if (!buttonHeldDown)
+           {
+               // Button has just been pressed, start tracking hold duration
+               buttonHeldDown = true;
+               actionExecuted = false;
+               holdStartTime = Time.time;
+           }
+           else if (!actionExecuted && Time.time - holdStartTime >= holdDurationThreshold) // Check if the hold duration exceeds the threshold
+           {
+                // Execute the action for holding the button down for the specified duration
+                modelLocked = ToggleLockBody(modelLocked);
+                PlayAudioCue();
+
+                // Set flag to indicate that action has been executed. 
+                actionExecuted = true;
+           }
+       }
+       else
+       {
+            // Button is released, reset button hold state
+            buttonHeldDown = false;
+            actionExecuted = false;
+       } 
+    }
+
+    bool ToggleLockBody(bool modelLocked)
+    {
+        // If model is not locked
+        if (!modelLocked){
+            // Lock the model
+            pressableButtons.OnReleaseSpineClicked();
+        }
+        else
+        {
+            // Unlock model if model is locked
+            pressableButtons.OnModifySpineClicked();
+        }
+
+        // Update model lock flag
+        return !modelLocked;
+    }
+
+    void MoveModelWithDPad()
+    {
+        if (Input.GetButton("Up"))
+        {
+
+        }
     }
 
     // Audio cues for button action exectuions
